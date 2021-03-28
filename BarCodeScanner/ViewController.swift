0e5e647e65951c1bd94dc15f77c59190b37c32a8
Дмитрий Кондратьев <konddima@gmail.com>
2021-03-28 11:01:33 +0300

    Fist commit

--- BarCodeScanner/ViewController.swift
+++ BarCodeScanner/ViewController.swift
@@ -4,16 +4,119 @@
 //
 //  Created by Дмитрий Кондратьев on 20.03.2021.
 //
+// https://barcode.tec-it.com/ru/Code93?data=ABC-1234-%2F%2B
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var barCodeLable: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createUILableBarCode()
+        
+        view.backgroundColor = UIColor.white
+        captureSession = AVCaptureSession()
+
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+
+        let metadataOutput = AVCaptureMetadataOutput()
+
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = metadataOutput.availableMetadataObjectTypes
+        } else {
+            failed()
+            return
+        }
+
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = CGRect(x: 0, y: 0, width: view.layer.frame.width, height: view.layer.frame.height-200)
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+        
+    }
+    
+    func createUILableBarCode() {
+        barCodeLable = UILabel(frame: CGRect(x:10, y: view.layer.frame.height-180, width: 200, height: 21))
+        //label.center = CGPointMake(160, 284)
+        //label.textAlignment = NSTextAlignment.Center
+        barCodeLable.text = "I'm a test label"
+        //view.layer.addSublayer(barCodeLable)
+        self.view.addSubview(barCodeLable)
+    }
+    
+    func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("4")
+        super.viewWillAppear(animated)
+        if (captureSession?.isRunning == false) {
+            captureSession.startRunning()
+            print("4.1")
+        }
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("5")
+        if (captureSession?.isRunning == true) {
+            captureSession.stopRunning()
+        }
+    }
 
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        //captureSession.stopRunning()
+        print("6")
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+
+        //dismiss(animated: true)
+    }
+
+    func found(code: String) {
+        print("1.1")
+        print(code)
+        barCodeLable.text = code
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+    
 }
 
